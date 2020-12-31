f2280269ab8484e077f95b61f64adabd5f52d6e5
Danijel Stracenski <danijel.stracenski@fer.hr>
2020-12-31 14:45:04 +0100

    Login works

---
+++ src/main/java/hr/fer/ruazosa/trackmyroute/TrackMyRouteApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.ruazosa.trackmyroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackMyRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackMyRouteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/hr/fer/ruazosa/trackmyroute/UserService.java
@@ -0,0 +1,44 @@
+package hr.fer.ruazosa.trackmyroute;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements IUserService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public boolean checkUsernameUnique(User user) {
+        List userList = userRepository.findByUserName(user.getUsername());
+        if (userList.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public User loginUser(UserBasic userBasic) {
+        List<User> loggedUserList = userRepository.findByUserNameAndPassword(userBasic.username, userBasic.password);
+        if (loggedUserList.isEmpty()) {
+            return null;
+        }
+        return userRepository.findByUserNameAndPassword(userBasic.username, userBasic.password).get(0);
+    }
+
+    @Override
+    public User getUserByUsernameAndPassword(UserBasic userBasic) {
+        List<User> loggedUserList = userRepository.findByUserNameAndPassword(userBasic.username,userBasic.password);
+        if (loggedUserList.isEmpty()) {
+            return null;
+        }
+        return userRepository.findByUserNameAndPassword(userBasic.username,userBasic.password).get(0);
+    }
+}
